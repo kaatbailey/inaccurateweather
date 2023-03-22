89dd6e6757d902a09a3b1514650f4768371c476d
kaatbailey <kaatlev420@gmail.com>
2023-03-22 15:04:00 -0400

    logging and icon handling
    Adding logging to the weather controller and weather service.
    modified icon handling and added handling for 26 more weather conditions.

--- src/main/java/com/kaat/inaccurateweather/WeatherController.java
+++ src/main/java/com/kaat/inaccurateweather/WeatherController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.CookieValue;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 @Controller
 public class WeatherController {
@@ -26,6 +27,10 @@ public class WeatherController {
 
     private final WeatherService weatherService;
 
+    // Create logger instance
+    private static final Logger logger = Logger.getLogger(String.valueOf(WeatherController.class));
+
+
     public WeatherController(Environment env, RestTemplate restTemplate, WeatherService weatherService) {
         this.env = env;
         this.restTemplate = restTemplate;
@@ -36,6 +41,9 @@ public class WeatherController {
     public String showWeather(Model model, @RequestParam(value = "zipCode", required = false) String zipCode,
                                @CookieValue(value = "jwt", required = false) String jwt,
                                HttpServletRequest request) {
+        // Log request parameters
+        logger.info("Received request for weather with zipCode=" + zipCode + " and jwt=" + jwt);
+
         String ipAddress = "98.97.24.220";;//Generic place holder address if we can't get one.
 
         //if no zipCode we'll try to pick the last good search one up from jwt.
@@ -46,6 +54,10 @@ public class WeatherController {
             if (zipCode != null) {
                 model.addAttribute("zipCode", zipCode);
             }
+
+            // Log zipCode retrieval from jwt or request parameters
+            logger.info("Retrieved zipCode=" + zipCode + " from jwt or request parameters");
+
 
             //if we don't have a zip rom jwt we'll try to get one via ipaddress to geolocation.
             if (zipCode == null || zipCode.isEmpty()) {
@@ -61,7 +73,8 @@ public class WeatherController {
         // Call GeolocateIPAddress method in WeatherService
         String apikey = env.getProperty("ipstack");
         zipCode = weatherService.GeolocateIPAddress(ipAddress, apikey);
-
+        // Log zipCode retrieval from ipaddress geolocation
+        logger.info("Retrieved zipCode=" + zipCode + " from ipaddress geolocation");
 
         //save any zipCode we have. if we don't have a cookie zip or a geolocate zip we'll have to use a default.
         if (!zipCode.isEmpty() || zipCode != null){
@@ -133,45 +146,69 @@ public class WeatherController {
         model.addAttribute("sunsetTime", sunsetTime);
         return "weather";
 
-
-
     }
 
-    //this just handles all of the images that go with the weather.
-    private String getIconFilename(String description) {
-        String filename = "";
-        switch (description) {
-            case "clear sky":
-                filename = "clear-day.svg";
-                break;
-            case "few clouds":
-                filename = "cloudy-1-day.svg";
-                break;
-            case "scattered clouds":
-            case "broken clouds":
-                filename = "cloudy-2-day.svg";
-                break;
-            case "shower rain":
-            case "rain":
-                filename = "rainy-1.svg";
-                break;
-            case "thunderstorm":
-                filename = "scattered-thunderstorms.svg";
-                break;
-            case "snow":
-            case "light snow":
-            case "heavy snow":
-                filename = "snowy-1.svg";
-                break;
-            case "mist":
-                filename = "rainy-3.svg";
-                break;
-            case "overcast clouds":
-                filename = "cloudy-3-day.svg";
-                break;
+    private static final Map<String, String> iconFilenames = new HashMap<>();
+    static {
+            iconFilenames.put("clear sky", "clear-day.svg");
+            iconFilenames.put("few clouds", "cloudy-1-day.svg");
+            iconFilenames.put("scattered clouds", "cloudy-2-day.svg");
+            iconFilenames.put("broken clouds", "cloudy-2-day.svg");
+            iconFilenames.put("shower rain", "rainy-1.svg");
+            iconFilenames.put("rain", "rainy-1.svg");
+            iconFilenames.put("thunderstorm", "scattered-thunderstorms.svg");
+            iconFilenames.put("snow", "snowy-1.svg");
+            iconFilenames.put("light snow", "snowy-1.svg");
+            iconFilenames.put("heavy snow", "snowy-1.svg");
+            iconFilenames.put("mist", "rainy-3.svg");
+            iconFilenames.put("overcast clouds", "cloudy-3-day.svg");
+            iconFilenames.put("moderate rain", "rainy-1.svg");
+            iconFilenames.put("light rain", "rainy-1.svg");
+            iconFilenames.put("haze", "haze-day.svg");
+            iconFilenames.put("fog", "fog-day.svg");
+            iconFilenames.put("smoke", "rainy-3.svg");
+            iconFilenames.put("dust", "rainy-3.svg");
+            iconFilenames.put("sand", "rainy-3.svg");
+            iconFilenames.put("ash", "rainy-3.svg");
+            iconFilenames.put("squall", "rainy-1.svg");
+            iconFilenames.put("tornado", "scattered-thunderstorms.svg");
+            iconFilenames.put("volcanic ash", "haze-day.svg");
+            iconFilenames.put("drizzle", "rainy-1.svg");
+            iconFilenames.put("freezing rain", "snow-and-sleet-mix.svg");
+            iconFilenames.put("heavy intensity rain", "rainy-1.svg");
+            iconFilenames.put("very heavy rain", "rainy-1.svg");
+            iconFilenames.put("extreme rain", "hurricane.svg");
+            iconFilenames.put("heavy intensity shower rain", "rainy-1.svg");
+            iconFilenames.put("ragged shower rain", "rainy-1.svg");
+            iconFilenames.put("light intensity drizzle", "rainy-1.svg");
+            iconFilenames.put("drizzle rain", "rainy-1.svg");
+            iconFilenames.put("heavy intensity drizzle", "rainy-1.svg");
+            iconFilenames.put("light intensity shower rain", "rainy-1.svg");
+            iconFilenames.put("shower snow", "snowy-1.svg");
+            iconFilenames.put("light shower snow", "snowy-1.svg");
+            iconFilenames.put("heavy shower snow", "snowy-1.svg");
+            iconFilenames.put("freezing drizzle", "snowy-1.svg");
+            iconFilenames.put("light rain and snow", "snowy-1.svg");
+            iconFilenames.put("rain and snow", "snowy-1.svg");
+            iconFilenames.put("light shower sleet", "snowy-1.svg");
+            iconFilenames.put("shower sleet", "snowy-1.svg");
+            iconFilenames.put("light rain and snow","snowy-1.svg");
+            iconFilenames.put("rain and snow","snowy-1.svg");
+            iconFilenames.put("light shower snow","snowy-1.svg");
+            iconFilenames.put("shower snow","snowy-1.svg");
+            iconFilenames.put("heavy shower snow","snowy-1.svg");
+            iconFilenames.put("smoke","fog.svg");
+            iconFilenames.put("haze","fog.svg");
+            iconFilenames.put("sand", "dust.svg");
+            iconFilenames.put("fog","fog.svg");
+            iconFilenames.put("sand","fog.svg");
+            iconFilenames.put("dust","dust.svg");
+            iconFilenames.put("volcanic ash","fog.svg");
         }
-        return filename;
-    }//what all weather conditions am i missing set a default?
+
+    private String getIconFilename(String description) {
+        return iconFilenames.getOrDefault(description, "tornado.svg");
+    }
 
 }
 
--- src/main/java/com/kaat/inaccurateweather/WeatherService.java
+++ src/main/java/com/kaat/inaccurateweather/WeatherService.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
+import java.util.logging.Logger;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,6 +14,9 @@ public class WeatherService {
 
     private final Environment env;
     private final RestTemplate restTemplate;
+    // Create logger instance
+    private static final Logger logger = Logger.getLogger(String.valueOf(WeatherController.class));
+
 
     @Autowired
     public WeatherService(Environment env, RestTemplate restTemplate) {
@@ -24,17 +27,23 @@ public class WeatherService {
 
     // Use ipstack API to get user's location using the user's IP address, but not localhost.
     public String GeolocateIPAddress(String ipaddress, String apikey) {
+        logger.info("GeolocateIPAddress called with ipaddress={} " + ipaddress);
         String ipstackUrl = "http://api.ipstack.com/" + ipaddress + "?access_key=" + env.getProperty("ipstack");
         IpstackResponse ipstackResponse = restTemplate.getForObject(ipstackUrl, IpstackResponse.class);
        String zipCode = ipstackResponse.getZip();
        if (!zipCode.isEmpty() && zipCode!=null){
+           logger.info("GeolocateIPAddress returned zipCode={}" + zipCode);
            return zipCode;
-       } else {return "notfound";}
+       } else {
+           logger.info("GeolocateIPAddress returned empty zipCode");
+           return "notfound";}
     }
 
     //OpenWeather only takes lon and lat for weather resolution instead of ipaddress, so resolve
     public OpenWeatherMapGeoResponse IpaddressToLonLatConvert(String zipCode, String apikey){
         // Use OpenWeatherMap Geo API to get latitude and longitude from zip code
+        logger.info("IpaddressToLonLatConvert called with zipCode={}" + zipCode);
+
         String geoUrl = "https://api.openweathermap.org/geo/1.0/zip?zip={zipCode},US&appid={apiKey}";
         Map<String, String> geoParams = new HashMap<>();
         geoParams.put("zipCode", zipCode); //CHANGE THIS BACK TO zipCode.
@@ -42,12 +51,16 @@ public class WeatherService {
 
         OpenWeatherMapGeoResponse geoResponse = restTemplate.getForObject(geoUrl, OpenWeatherMapGeoResponse.class, geoParams);
         //if geoResponse not empty then we'll return it.
+        logger.info("IpaddressToLonLatConvert returned geoResponse={} " + geoResponse);
+
         return geoResponse;
 
     }
 
     //pass in lon and lat and get day of weather
     public WeatherResponse GetWeatherFromLonLat(OpenWeatherMapGeoResponse geoResponse, String zipCode, String apikey){
+        logger.info("GetWeatherFromLonLat called with geoResponse={}, and zipCode={} " + geoResponse + " " + zipCode);
+
         String openWeatherMapUrl = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}&units=imperial";
         // FOR A 5 DAY WE WOULD USE THE WORD FORCAST INSTEAD OF weather?lat etc.
         Map<String, String> forecastParams = new HashMap<>();
@@ -55,7 +68,7 @@ public class WeatherService {
         forecastParams.put("lon", String.valueOf(geoResponse.getLon()));
         forecastParams.put("apiKey", env.getProperty("openweathermap"));
         WeatherResponse response = restTemplate.getForObject(openWeatherMapUrl, WeatherResponse.class, forecastParams);
-
+        logger.info("GetWeatherFromLonLat returned response={} " + response);
         return response;
     }
 
